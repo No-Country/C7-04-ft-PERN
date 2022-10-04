758e151e18f00d5359d279a1394820010132f560
MATIAS TORRES <matiiiastorres@gmail.com>
2022-10-04 00:36:21 -0300

    mod footer

--- no-country-fe/pages/index.jsx
+++ no-country-fe/pages/index.jsx
@@ -1,4 +1,5 @@
 import AsideNav from 'src/components/utils/AsideNav'
+import Footer from 'src/components/utils/Footer'
 import styles from '../styles/Home.module.css'
 import { mainAsideNavSections } from 'helpers/aside-nav';
 
@@ -6,6 +7,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <AsideNav sections={mainAsideNavSections} />
+      <Footer/>
     </div>
   )
 }
