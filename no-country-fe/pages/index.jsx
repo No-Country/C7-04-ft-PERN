40e478046f67e9c0e42fa3d5c92932a9a2013ccb
betobogino <beto.bogino@gmail.com>
2022-10-05 01:11:43 -0300

    ADD: Header

--- no-country-fe/pages/index.jsx
+++ no-country-fe/pages/index.jsx
@@ -1,10 +1,17 @@
-import AsideNav from 'src/components/utils/AsideNav'
+import AsideNav from 'src/components/utils/AsideNav';
+import Header from 'src/components/utils/Header';
+import HeaderCategorys from 'src/components/utils/HeaderCategorys';
+
 import { mainAsideNavSections } from 'helpers/aside-nav';
 
 export default function Home() {
   return (
-    <div>
+    <div className='main'>
       <AsideNav sections={mainAsideNavSections} />
+      <div>
+        <Header />
+        <HeaderCategorys />
+      </div>
     </div>
   )
 }
